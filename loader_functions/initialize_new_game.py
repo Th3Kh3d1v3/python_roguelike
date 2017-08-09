0d622b1409a6f2928054298d53e513e4c96388aa
Musaab <musaab@users.noreply.github.com>
2017-08-09 11:50:31 +0300

    Finished Tutorial 12

--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -34,9 +34,6 @@ def get_constants():
     fov_light_walls = True
     fov_radius = 10
 
-    max_monsters_per_room = 3
-    max_items_per_room = 2
-
     colors = {
         'dark_wall': (0, 0, 100),
         'dark_ground': (50, 50, 150),
@@ -79,8 +76,6 @@ def get_constants():
         'fov_algorithm': fov_algorithm,
         'fov_light_walls': fov_light_walls,
         'fov_radius': fov_radius,
-        'max_monsters_per_room': max_monsters_per_room,
-        'max_items_per_room': max_items_per_room,
         'colors': colors
     }
 
@@ -89,7 +84,7 @@ def get_constants():
 
 def get_game_variables(constants):
     humanoid_component = Humanoid(race=Races.Human, profession=Professions.Fighter)
-    fighter_component = Fighter(hp=30, defense=2, power=5)
+    fighter_component = Fighter(hp=100, defense=1, power=4)
     inventory_component = Inventory(26)
     level_component = Level()
     player = Entity(0, 0, '@', (255, 255, 255), 'Musaab', blocks=True, render_order=RenderOrder.ACTOR,
@@ -99,8 +94,7 @@ def get_game_variables(constants):
 
     game_map = GameMap(constants['map_width'], constants['map_height'])
     make_map(game_map, constants['max_rooms'], constants['room_min_size'],
-             constants['room_max_size'], constants['map_width'], constants['map_height'], player,
-             entities, constants['max_monsters_per_room'], constants['max_items_per_room'],
+             constants['room_max_size'], constants['map_width'], constants['map_height'], player, entities,
              constants['colors'])
 
     message_log = MessageLog(constants['message_x'], constants['message_width'],
